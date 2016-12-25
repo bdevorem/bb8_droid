07f91a09d923abb0fe263f81835577dc48a35bca
bdevorem <bdevorem@nd.edu>
2016-12-25 18:48:43 -0500

    add readme, libraries, colors scripts

---
+++ README.md
@@ -0,0 +1,6 @@
+# bb8_droid
+
+Series of Python scripts and libraries to play around with my 
+new Sphero BB8 Droid.  
+
+`BB8_driver.py` and `BB8_simple.py` from @jchadwhite
---
+++ colors.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from bluepy import btle
+import struct
+import time
+import BB8_driver
+import sys
+
+bb8 = BB8_driver.Sphero()
+
+print "connecting...\n"
+bb8.connect()
+
+print "starting...\n"
+bb8.start()
+time.sleep(2)
+
+print "goin red...\n"
+bb8.set_rgb_led(255, 0, 0, 0, False)
+time.sleep(1)
+
+print "goin green...\n"
+bb8.set_rgb_led(0, 255, 0, 0, False)
+time.sleep(1)
+
+print "goin blue...\n"
+bb8.set_rgb_led(0, 0, 255, 0, False)
+time.sleep(1)
+
+print "goin neon yellow...\n"
+bb8.set_rgb_led(255, 255, 0, 0, False)
+time.sleep(1)
+
+print "goin purple...\n"
+bb8.set_rgb_led(255, 0, 255, 0, False)
+time.sleep(1)
+
+print "goin pink...\n"
+bb8.set_rgb_led(255, 0, 127, 0, False)
+time.sleep(1)
+
+print "goin turquoise...\n"
+bb8.set_rgb_led(0, 255, 255, 0, False)
+time.sleep(1)
+
+print "disconnecting...\n"
+bb8.join()
+bb8.disconnect()
+sys.exit(1)
+
